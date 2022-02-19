cdbe464e0370b80ce88e60fc07fd003aa9cec286
Alexa J <alexadoestuff@protonmail.com>
2022-02-19 10:34:37 -0500

    KFT-00 | base files

---
+++ app.js
@@ -0,0 +1,9 @@
+/* App.js
+* Main entry point to this practice repo... not sure what I'm going to make but I'd
+* like to practice coding 
+*/
+
+const rootEl = document.getElementById('app');
+const paragraphEle = document.createElement('p');
+paragraphEle.innerText = 'Hello there';
+rootEl.appendChild(paragraphEle);
